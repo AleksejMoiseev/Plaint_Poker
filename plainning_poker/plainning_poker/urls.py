44b75d9a88ed93bc309a04bedff36ab71ef4812f
Aleksej Moiseev <moiseev.003@bk.ru>
2021-08-27 17:10:39 +0300

    add handler404

--- plainning_poker/plainning_poker/urls.py
+++ plainning_poker/plainning_poker/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from poker.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('poker.urls')),
 ]
+
+handler404 = page_not_found
--- plainning_poker/poker/views.py
+++ plainning_poker/poker/views.py
@@ -3,6 +3,8 @@ from django.http.response import HttpResponse
 from django.views.generic.edit import CreateView
 from poker.forms import RegisterUserForm
 from django.urls import reverse_lazy
+from django.shortcuts import render
+from rest_framework import status
 
 
 # Create your views here.
@@ -17,4 +19,12 @@ class RegisterUser(CreateView):
     template_name = 'register.html'
     extra_context = {'title': 'Register'}
     success_url = reverse_lazy('/test/')
+
+
+def page_not_found(request, exceptions):
+    render(request=request, template_name='error_404.html', status=status.HTTP_404_NOT_FOUND)
+
+
+def unauthorized_error(request, exceptions):
+    render(request=request, template_name='error_401.html', status=status.HTTP_401_UNAUTHORIZED)
 
